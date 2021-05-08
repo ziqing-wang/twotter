988bf6fe15c219ad02a0b98f69f6d825a7c63a95
Zi <zileaderr@gmail.com>
2021-05-08 16:30:40 +0200

    add server.js file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const serverStatic = require('serve-static');
+const history = require('connect-history-api-fallback');
+const enforce = require('express-sslify')
+
+const app = express();
+
+app.use(enforce.HTTPS({
+    trustProtoHeader: true
+}));
+app.use(serverStatic(__dirname + '/dist'));
+app.use(history())
+
+app.listen(process.env.PORT || 5000);
